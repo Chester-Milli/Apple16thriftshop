e7a071d9d3e5075b03ffcfb2920d245dc532dc7d
Chiemena <aliachiemena@gmail.com>
2022-11-13 22:11:16 -0800

    first commit

---
+++ Style/index.js
@@ -0,0 +1,13 @@
+var x  = 300.5 ;
+var y = 300;
+var a = y
+var e = 200
+var sentence = "Hello world, my name is Chiemena, I am a programmer, a web developer";
+var ans  = x + y + a + e;
+console.log(sentence)
+
+
+var age = 10;
+var myage = 25;
+var realage = age + myage;
+ console.log(realage)
